5d79b017ec8548910c34316e3d09f53a77bddc90
Leigh London <leigh.00.london@gmail.com>
2020-04-25 22:38:18 +1000

    update the logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
-die() { echo "::error file=entrypoint.sh::$1" >&2; exit "${2:-1}"; }
+die() { echo "::error file=entrypoint.sh:: $1" >&2; exit "${2:-1}"; }
 hash hubr 2>/dev/null || die "missing hubr"
 export HUBR_DEFAULT_ORG=""
 
-tags=$(hubr tags "$1")
-echo "::set-output name=tags:: $tags"
-echo "$tags"
-echo "::warning:: a warning"
+# github repo name needs to be provided, e.g. "org/repo"
+repo=${GITHUB_REPOSITORY:?missing repository from environment}
+# hubr will read the version from the "VERSION" file by default.
+file=${INPUT_FROM:-VERSION}
+
+# set the draft flag by default.
+opts="-d"
+hubr now && opts=""
+
+version=$(head -n1 "$file") || die "unable to read version file"
+hubr push "$opts" "$name"   || die "unable to push release"
+
+echo "::set-output name=version:: $version"
+echo "released $repo@$version"
